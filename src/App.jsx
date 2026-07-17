7d09e49ee21de42217ddcb4a519b1e3242aea7d4
Rushil Koul <ruskoul3@gmail.com>
2026-07-17 23:57:37 +0530

    compose app from components

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+
+import Preloader from "./components/preloader/preloader";
+import Navbar from "./components/navbar/navbar";
+import Landing from "./components/landing-page/landing";
+import About from "./components/about/about";
+import Highlights from "./components/highlights/highlights";
+import Events from "./components/events/events";
+import Gallery from "./components/gallery/gallery";
+import Team from "./components/team/team";
+import WIE from "./components/WIE/WIE";
+import Footer from "./components/footer/footer";
+
+function App() {
+    return (
+        <>
+            {/* preload logic badmei daldena. appended it here for now */}
+            {/* <Preloader /> */}
+
+            <Navbar />
+
+            <Landing />
+            <Highlights />
+            <About />
+            <Events />
+            <WIE />
+            <Team />
+            <Gallery />
+
+            <Footer />
+        </>
+    );
+}
+
+export default App;
