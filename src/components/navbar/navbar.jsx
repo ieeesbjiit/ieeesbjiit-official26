d01cdbca8f7dfb8b347d520792213a3f2ab7087b
naman101222 <153426627+naman101222@users.noreply.github.com>
2026-07-24 00:09:07 +0530

    first temp navbar ,was learning how to work with react

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,26 +1,50 @@
-import React from 'react'
+import { useState } from "react";
+import "./navbar.css";
+//import logo from "./ieeelogo.png"; // change path if logo is elsewhere
 
-// putting up a very basic navbar to make the landing more cohesive.
-// naman will change this later
-const navbar = () => {
-  return (
-    <>
-      <div className='navbar'>
-        <div>
-          IEESBJIIT
-        </div>
+const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
 
-        <div>
-          <span>Highlights</span>
-          <span>About</span>
-          <span>Events</span>
-          <span>WIE</span>
-          <span>Team</span>
-          <span>Gallery</span>
-        </div>
-      </div>
-    </>
-  )
-}
+    return (
+        <nav className="ieee-navbar">
+            <div className="ieee-navbar-container">
 
-export default navbar
+                {/* Replace this with logo later */}
+                <div className="ieee-logo">
+                    IEEE SB JIIT
+                </div>
+
+                <button
+                    className={`ieee-navbar-toggle ${menuOpen ? "active" : ""}`}
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >
+                    <span className="ieee-bar"></span>
+                    <span className="ieee-bar"></span>
+                    <span className="ieee-bar"></span>
+                </button>
+
+                <ul className={`ieee-navbar-menu ${menuOpen ? "active" : ""}`}>
+
+                    <li><a href="#home">HOME</a></li>
+
+                    <li><a href="#about">ABOUT</a></li>
+
+                    <li><a href="#events">EVENTS</a></li>
+
+                    <li><a href="#wie">WIE</a></li>
+
+                    <li><a href="#team">TEAM</a></li>
+
+                    <li><a href="#gallery">GALLERY</a></li>
+
+                    <li><a href="#contact">CONTACT US</a></li>
+
+                </ul>
+
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
+
