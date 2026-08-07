d7ab295453dd484bb31ed2a16373880fa7995e4d
Janmejai Pandey <janmejai.pandey0403@gmail.com>
2026-08-07 23:30:01 +0530

    Added 3D pop effect

--- src/components/team/data/teamData.js
+++ src/components/team/data/teamData.js
@@ -1,136 +1,177 @@
-import rayansh from "./Team posters/rayansh.png"
-import abhay from "./Team posters/abhay sir.png"
-import adhirath from "./Team posters/adhirath sir.png"
-import bani from "./Team posters/bani.png"
-import ansh from "./Team posters/ansh.png"
-import anish from "./Team posters/anish.png"
-import edhita from "./Team posters/edhita.png"
-import eishit from "./Team posters/eishit.png"
-import saarang from "./Team posters/saarang.png"
-import khushi from "./Team posters/khushi.png"
-import yashaswini from "./Team posters/yashaswini.png"
-import lakshitaa from "./Team posters/lakshitaa.png"
-import navyaa from "./Team posters/navyaa.png"
-import hardik from "./Team posters/hardik.png"
-import anika from "./Team posters/anika.png"
-import kriti from "./Team posters/kriti.png"
-import garv from "./Team posters/garv.png"
-import navya from "./Team posters/navya.png"
-import aanya from "./Team posters/aanya.png"
+// Full photos
+import rayansh from "./Team posters/rayansh.png";
+import abhay from "./Team posters/abhay sir.png";
+import adhirath from "./Team posters/adhirath sir.png";
+import bani from "./Team posters/bani.png";
+import ansh from "./Team posters/ansh.png";
+import anish from "./Team posters/anish.png";
+import edhita from "./Team posters/edhita.png";
+import eishit from "./Team posters/eishit.png";
+import saarang from "./Team posters/saarang.png";
+import khushi from "./Team posters/khushi.png";
+import yashaswini from "./Team posters/yashaswini.png";
+import lakshitaa from "./Team posters/lakshitaa.png";
+import navyaa from "./Team posters/navyaa.png";
+import hardik from "./Team posters/hardik.png";
+import anika from "./Team posters/anika.png";
+import kriti from "./Team posters/kriti.png";
+import garv from "./Team posters/garv.png";
+import navya from "./Team posters/navya.png";
+import aanya from "./Team posters/aanya.png";
+
+// Cutout photos (person only, transparent background)
+import rayansh_o from "./Team posters/rayansh_o.png";
+import abhay_o from "./Team posters/abhay sir_o.png";
+import adhirath_o from "./Team posters/bani_o.png";
+import bani_o from "./Team posters/bani_o.png";
+import ansh_o from "./Team posters/ansh_o.png";
+import anish_o from "./Team posters/anish_o.png";
+import edhita_o from "./Team posters/edhita_o.png";
+import eishit_o from "./Team posters/eishit_o.png";
+import saarang_o from "./Team posters/saarang_o.png";
+import khushi_o from "./Team posters/khushi_o.png";
+import yashaswini_o from "./Team posters/yashaswini_o.png";
+import lakshitaa_o from "./Team posters/lakshitaa_o.png";
+import navyaa_o from "./Team posters/navyaa_o.png";
+import hardik_o from "./Team posters/hardik_o.png";
+import anika_o from "./Team posters/anika_o.png";
+import kriti_o from "./Team posters/kriti_o.png";
+import garv_o from "./Team posters/garv_o.png";
+import navya_o from "./Team posters/navya_o.png";
+import aanya_o from "./Team posters/aanya_o.png";
 
 const TEAM = [
   {
     name: "Dr. Abhay Kumar",
     role: "Branch Counselor",
     img: abhay,
+    cutout: abhay_o,
     linkedin: "https://www.linkedin.com/in/janmejai-pandey-050b56387",
   },
   {
     name: "Dr. Adhirath Mandal",
     role: "Branch Counselor",
     img: adhirath,
+    cutout: adhirath_o,
     linkedin: "https://www.linkedin.com/in/priya-nair",
   },
   {
     name: "Rayansh Gupta",
     role: "Chairperson",
     img: rayansh,
+    cutout: rayansh_o,
     linkedin: "https://www.linkedin.com/in/rohan-sharma",
   },
   {
     name: "Bani Gupta",
     role: "Vice Chairperson",
     img: bani,
+    cutout: bani_o,
     linkedin: "https://www.linkedin.com/in/sneha-reddy",
   },
   {
     name: "Ansh Goyal",
     role: "Organization Secretary",
     img: ansh,
+    cutout: ansh_o,
     linkedin: "https://www.linkedin.com/in/kiran-patel",
   },
   {
     name: "Anish Dutta",
     role: "Treasurer",
     img: anish,
+    cutout: anish_o,
     linkedin: "https://www.linkedin.com/in/ananya-iyer",
   },
   {
     name: "Edhita Nakura",
     role: "WIE Head",
     img: edhita,
+    cutout: edhita_o,
     linkedin: "https://www.linkedin.com/in/vikram-das",
   },
   {
     name: "Eishit Balyan",
     role: "Strategic Head",
     img: eishit,
+    cutout: eishit_o,
     linkedin: "https://www.linkedin.com/in/meera-pillai",
   },
   {
     name: "Saarang Khanna",
     role: "Logistics Head",
     img: saarang,
+    cutout: saarang_o,
     linkedin: "https://www.linkedin.com/in/rahul-verma",
   },
   {
     name: "Khushi Mittal",
     role: "Technical CSE Head",
     img: khushi,
+    cutout: khushi_o,
     linkedin: "https://www.linkedin.com/in/divya-krishnan",
   },
   {
     name: "Yashaswini Mahanta",
     role: "Technical ECE Head",
     img: yashaswini,
+    cutout: yashaswini_o,
     linkedin: "https://www.linkedin.com/in/aditya-bose",
   },
   {
     name: "Lakshitaa Parashar",
     role: "Webmaster",
     img: lakshitaa,
+    cutout: lakshitaa_o,
     linkedin: "https://www.linkedin.com/in/lakshmi-menon",
   },
   {
     name: "Navyaa Jain",
     role: "Digital Head",
     img: navyaa,
+    cutout: navyaa_o,
     linkedin: "https://www.linkedin.com/in/suresh-rao",
   },
   {
     name: "Hardik Dixit",
     role: "Cinematography Head",
     img: hardik,
+    cutout: hardik_o,
     linkedin: "https://www.linkedin.com/in/pooja-gupta",
   },
   {
     name: "Anika Tomar",
     role: "Marketing Head",
     img: anika,
+    cutout: anika_o,
     linkedin: "https://www.linkedin.com/in/nikhil-joshi",
   },
   {
     name: "Kriti Walecha",
     role: "Public Relations Head",
     img: kriti,
+    cutout: kriti_o,
     linkedin: "https://www.linkedin.com/in/kavya-shetty",
   },
   {
     name: "Garv Batra",
     role: "Management Head",
     img: garv,
+    cutout: garv_o,
     linkedin: "https://www.linkedin.com/in/manish-tiwari",
   },
   {
     name: "Navya Gupta",
     role: "Creative Head",
     img: navya,
+    cutout: navya_o,
     linkedin: "https://www.linkedin.com/in/isha-kapoor",
   },
   {
     name: "Aanya Mahajan",
     role: "Campus Outreach Head",
     img: aanya,
+    cutout: aanya_o,
     linkedin: "https://www.linkedin.com/in/deepak-nambiar",
   },
 ];
