d74a5d04dc62bd39250c007fcd7dad177a415702
naman101222 <153426627+naman101222@users.noreply.github.com>
2026-08-10 19:56:27 +0530

    gallery nav fixed

--- src/components/StackedDeckGallery.jsx
+++ src/components/StackedDeckGallery.jsx
@@ -190,7 +190,7 @@ function StackedDeckGallery({ photos = defaultPhotos }) {
   }, [activeIndex]);
 
   return (
-    <div className="deck-gallery-wrap">
+    <div  id= "gallery" className="deck-gallery-wrap">
       <div className="masthead">
         <div className="masthead-top">
           <div>
