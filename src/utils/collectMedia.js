dd4956eb3f32c97a7da14e9f2dd79481317a7c84
Janmejai Pandey <janmejai.pandey0403@gmail.com>
2026-08-11 18:16:27 +0530

    Preloader now loads everything.
    Added images references to the preloader.

--- src/utils/collectMedia.js
+++ src/utils/collectMedia.js
@@ -1,6 +1,27 @@
 // Team photos
 import TEAM from '../components/team/data/teamData';
-import logo from '../assets/ieee_white.png';
+import logo from '../assets/ieee_logo.jpeg';
+import ieeeWhite from '../assets/ieee_white.png';
+import bg from '/back.png';
+
+//Gallery images
+import gallery1 from '/gallery/optimized/Copy of 20260404_141808.jpg';
+import gallery2 from '/gallery/optimized/Copy of Copy of IMG_9502.jpg';
+import gallery3 from '/gallery/optimized/Copy of DSC09810.jpg';
+import gallery4 from '/gallery/optimized/Copy of DSC09845.jpg';
+import gallery5 from '/gallery/optimized/Copy of DSC09960.jpg';
+import gallery6 from '/gallery/optimized/Copy of IMG_0164.jpg';
+import gallery7 from '/gallery/optimized/Copy of IMG_20250915_172953.jpg';
+import gallery8 from '/gallery/optimized/Copy of IMG_20250915_181256_1.jpg';
+
+//Events images
+import event1 from '../components/events/posters/ieee_day.jpeg';
+import event2 from '../components/events/posters/lumina.PNG';
+import event3 from '../components/events/posters/research_forum.jpeg';
+import event4 from '../components/events/posters/techblocks.jpg';
+import event5 from '../components/events/posters/webinar.PNG';
+import event6 from '../components/events/posters/workshop.jpeg';
+import event7 from '../components/events/posters/xenith.PNG';
 
 // Highlights images
 import attendees from '../components/highlights/attendees.jpg';
@@ -11,9 +32,17 @@ import Xenith from '../components/highlights/Xenith.PNG';
 // Footer image
 import floor from '../components/footer/assets/ocean-floor.png';
 
-// Landing video (uncomment when you have it)
+// Landing video
 import landingVideo from '../components/landing-page/underwater.mp4';
 import logoVideoWebm from '../components/landing-page/logo.webm';
+
+//WIE images
+import wieLogo from "../components/WIE/images/wie-logo.png";
+import slide1 from "../components/WIE/images/slide1.jpg";
+import slide2 from "../components/WIE/images/slide2.jpg";
+import slide3 from "../components/WIE/images/slide3.jpg";
+import slide4 from "../components/WIE/images/slide4.jpg";
+import slide5 from "../components/WIE/images/slide5.jpg";
 
 /**
  * Returns an array of all media files to preload with URL + estimated weight.
@@ -25,7 +54,8 @@ export function getAllMediaSources() {
 
   // ---------- IMAGES ----------
   // Static images (small — average 100-300KB each)
-  const staticImages = [attendees, members, WIE, Xenith, logo, floor];
+  const staticImages = [attendees, members, WIE, Xenith, logo, floor, ieeeWhite, bg, wieLogo, slide1, slide2, slide3, slide4, slide5, gallery1, gallery2, gallery3, gallery4, gallery5, gallery6, gallery7, gallery8,
+    event1, event2, event3, event4, event5, event6, event7];
   staticImages.forEach((url) => {
     sources.push({ url, weight: 4000000 }); // ~4MB estimate
   });
